ddf4013ad7fe0c701c6e900dde9b5b47e31af960
SolarCristobal <88000744+SolarCristobal@users.noreply.github.com>
2025-06-01 20:08:20 -0400

    Update weather.ts

--- Proyecto_Semestral/src/types/weather.ts
+++ Proyecto_Semestral/src/types/weather.ts
@@ -1,16 +1,21 @@
 /* Datos accesibles, de querer más se debe modificar y agregar */
 export type WeatherData = {
-    name: string;
-    coord: { lat: number; lon: number };
-    main: { temp: number; humidity: number };
-    weather: { description: string; icon: string; main: string }[];
-    wind: { speed: number };
+  name: string;
+  coord: { lat: number; lon: number };
+  main: { temp: number; humidity: number };
+  weather: { description: string; icon: string; main: string }[];
+  wind: { speed: number };
+  visibility?: number; 
+  rain?: {
+    '1h'?: number; // OpenWeatherMap puede dar lluvia en 1h o 3h
+    '3h'?: number;
   };
-  
-  export type ForecastData = {
-    list: {
-      dt_txt: string;
-      main: { temp: number };
-      weather: { icon: string; description: string; main: string }[];
-    }[];
-  };
+};
+
+export type ForecastData = {
+  list: {
+    dt_txt: string;
+    main: { temp: number };
+    weather: { icon: string; description: string; main: string }[];
+  }[];
+};
